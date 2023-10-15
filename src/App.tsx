5653b90f2f2cee039a789527b6d25aa0452868be
techpilot <ngwustephen99@gmail.com>
2023-10-15 09:17:08 +0100

    prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Todo from './components/Todo';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      Todo List
-    </>
-  )
+    <div>
+      <Todo />
+    </div>
+  );
 }
 
-export default App
+export default App;
